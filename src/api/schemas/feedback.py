a990315a717872b3619999b26dc2f9ba6d469425
Tatevik Harutyunyan <tatevik.harutyunyan@armenia.talkdesk.com>
2022-03-31 14:05:26 +0400

    update

--- src/api/schemas/feedback.py
+++ src/api/schemas/feedback.py
@@ -1,15 +1,28 @@
+from datetime import datetime
 from typing import Optional
 
 from pydantic import BaseModel
 
 
-class Feedback(BaseModel):
+class FeedbackCreate(BaseModel):
     full_name: str
     phone_number: str
     email: str
     score: int
     comment: Optional[str] = None
     waiter_id: Optional[int] = None
+
+    class Config:
+        orm_mode = True
+
+class FeedbackRetrieve(BaseModel):
+    full_name: str
+    phone_number: str
+    email: str
+    score: int
+    comment: Optional[str] = None
+    waiter_name: Optional[str] = None
+    time_created: Optional[datetime] = None
 
     class Config:
         orm_mode = True
--- src/db/postgres/crud/feedback_crud.py
+++ src/db/postgres/crud/feedback_crud.py
@@ -2,10 +2,12 @@ from sqlalchemy.orm import Session
 
 from src.api.schemas import FeedbackSchema
 from src.models.feedback import Feedback
+from src.models.waiter import Waiter
 
 
 class FeedbackCRUD:
-    model_class = Feedback
+    model_feedback_class = Feedback
+    model_waiter_class = Waiter
 
     @classmethod
     async def create_feedback(cls, db: Session, feedback: FeedbackSchema):
@@ -21,10 +23,45 @@ class FeedbackCRUD:
         db.commit()
         db.refresh(db_feedback)
         return db_feedback
-    
+
     @classmethod
     async def delete_feedback_by_full_name(cls, db: Session, name: str):
         feedback = db.query(Feedback).filter_by(full_name=name).first()
         db.delete(feedback)
         db.commit()
 
+    @classmethod
+    async def list_feedback(cls, db: Session, score_filter=None, has_free_comment=None):
+        #feedback_cursor = db.query(
+        #    cls.model_feedback_class.full_name,
+        #    cls.model_feedback_class.phone_number,
+        #    cls.model_feedback_class.email,
+        #    cls.model_feedback_class.time_created,
+        #    cls.model_feedback_class.comment,
+        #    cls.model_feedback_class.score,
+        #    cls.model_feedback_class.waiter_id,
+        #    cls.model_waiter_class.name.label("waiter_name"),
+        #).join(
+        #    cls.model_waiter_class,
+        #    cls.model_feedback_class.waiter_id == cls.model_waiter_class.id,
+        #    isouter=True,
+        #)
+        feedback_cursor = db.query(cls.model_feedback_class)
+
+        if score_filter:
+            feedback_cursor = feedback_cursor.filter(
+                cls.model_feedback_class.score.in_(score_filter)
+            )
+
+        if has_free_comment:
+            feedback_cursor = feedback_cursor.filter(
+                cls.model_feedback_class.comment != None
+            )
+
+        #feedback_cursor = db.query(
+        #        .waiter
+        #        )
+
+        feedback_count = feedback_cursor.count()
+
+        return feedback_cursor, feedback_count
